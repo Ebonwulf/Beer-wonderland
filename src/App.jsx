6c0baefb9b328edcee3f62fd4ad47c573b44b71f
Lyssa <ebonwolf00@gmail.com>
2022-04-20 15:59:48 +0100

    file structure created and all jsx and scss files created and in the correct folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
 import './App.scss';
-
+import beers from './data/data';
 const App = () => {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <></>;
 };
 
 export default App;
